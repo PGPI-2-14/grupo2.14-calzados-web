12c873f62e14bc8182864dd54949ab733bee7f6f
Pablo Ferreria <pferreriahijon@gmail.com>
2025-11-19 00:05:15 +0100

    Revert "Test SendGrid"
    This reverts commit 5ded51ea26cc55a28df596fe0106d4b04f54aee5.

--- order/urls.py
+++ order/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('create/', views.order_create, name='order_create'),
     path('payment/<int:order_id>/', views.payment_process, name='payment_process'),
     path('created/<int:order_id>/', views.order_created, name='order_created'),
-    path('ticket/<int:order_id>/', views.download_ticket, name='download_ticket'),
 ]
